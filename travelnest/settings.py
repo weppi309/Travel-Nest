ade73b3e3042b06abd0b19a100c049652068f4c1
phanque <phanque.0923@gmail.com>
2024-06-19 23:52:39 +0700

    updatesetting

--- travelnest/settings.py
+++ travelnest/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mb&oey%hnm5*v16mi$4t&4v3upx(41_g7o%2lk@oa3t68lpejl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
