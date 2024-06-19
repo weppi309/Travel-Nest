255d7242a0001581fbea4c582521cfd1d35e21ba
phanque <phanque.0923@gmail.com>
2024-06-19 23:38:31 +0700

    csrf

--- app/middleware.py
+++ app/middleware.py
@@ -50,9 +50,9 @@ class RoleBasedAccessMiddleware:
 
 from django.utils.deprecation import MiddlewareMixin
 
-class NoCacheMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
-        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-        response['Pragma'] = 'no-cache'
-        response['Expires'] = '0'
-        return response
+# class NoCacheMiddleware(MiddlewareMixin):
+#     def process_response(self, request, response):
+#         response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+#         response['Pragma'] = 'no-cache'
+#         response['Expires'] = '0'
+#         return response
