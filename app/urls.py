3f9a49f56fe5bc60e20181b0b0330098ed5df248
nguyentynhiii <2051012082nhi@ou.edu.vn>
2024-06-21 13:23:20 +0700

    add payment

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,13 @@ urlpatterns = [
     # # path('custom-profile/', views.profile, name='custom_profile'),
     # path('customer-dashboard/', views.dashboard, name='customer_dashboard'),
     # path('edit-profile/', views.edit_profile, name='edit_profile'),
+
+    path('pay', views.index_payment, name='index'),
+    path('payment', views.payment, name='payment'),
+    path('payment_ipn', views.payment_ipn, name='payment_ipn'),
+    path('payment_return', views.payment_return, name='payment_return'),
+    path('query', views.query, name='query'),
+    path('refund', views.refund, name='refund'),
 ]
 
 urlpatterns +=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
